fa766c3110cfa8157a2d4f5fed96ec63836d47dc
kavin413 <srisudhakavin@gmail.com>
2025-09-22 08:25:33 +0530

    Add files via upload

--- pyhton.py
+++ pyhton.py
@@ -1,9 +1,18 @@
-import turtle 
-square_turtle = turtle.Turtle()
 
-side_length = 100
-for _ in range(4):
-    square_turtle.forward(side_length) 
-    square_turtle.left(90)
+try:
+    number = int(input("Enter an integer: "))
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+else:
 
-turtle.done()
+    if number > 0:
+        print(f"{number} is a positive number.")
+    elif number < 0:
+        print(f"{number} is a negative number.")
+    else:
+        print(f"{number} is zero.")
+
+    if number % 2 == 0:
+        print(f"{number} is an even number.")
+    else:
+        print(f"{number} is an odd number.")
